b3263f6cf7c3eeb9aeca4d3f75aa7e3690f6974e
artem <mr.artnov@mail.ru>
2022-07-16 21:28:39 +0300

    end

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 module.exports = function reverse (n) {
-    let result = 0;
-    while (n) {
-        result = result * 10 + n % 10;
-        n = Math.floor(n / 10);
+    const str = (Math.abs(n)).toString()
+    let res = ''
+    const isNegative = n < 0
+    
+    for (let i = 0; i < str.length; i++){
+      const char = str[i]
+      if (char !== '0') res = char + res
+      else if (char === '0' && str[i + 1] && str[i + 1] !== '0') res = char + res
     }
-
-    return result;
-    console.log(reverse(n));
-}
+  
+    return Number(isNegative ? res * 1 : res)
+  }
